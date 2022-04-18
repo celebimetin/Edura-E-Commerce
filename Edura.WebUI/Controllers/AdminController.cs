84f742f0528fbcb59a5ce91ca8eddd4325ed7a90
Metin Celebi <metincelebi5534@gmail.com>
2022-04-18 11:42:35 +0300

    Administration - Catalog - 1 done.

---
+++ Edura.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Edura.WebUI.Models;
+using Edura.WebUI.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Edura.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _productRepository;
+        private ICategoryRepository _categoryRepository;
+
+        public AdminController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CatalogList()
+        {
+            var model = new CatalogListModel()
+            {
+                Categories = _categoryRepository.GetAll().ToList(),
+                Products = _productRepository.GetAll().ToList()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Edura.WebUI/Models/CatalogListModel.cs
@@ -0,0 +1,11 @@
+﻿using Edura.WebUI.Entity;
+using System.Collections.Generic;
+
+namespace Edura.WebUI.Models
+{
+    public class CatalogListModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
